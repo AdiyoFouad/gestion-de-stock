5cfd67fd8c65fedc42efa98a42fb9e781f3da8a8
AdiyoFouad <odjfouad38@gmail.com>
2025-11-10 10:58:48 +0100

    first commit

---
+++ src/test/java/com/gestion_de_stock/produit/GestionDeStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestion_de_stock.produit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
