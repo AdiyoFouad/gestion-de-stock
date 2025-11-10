446930eb222a4b017e15b7767821fac22fa734f8
AdiyoFouad <odjfouad38@gmail.com>
2025-11-10 16:13:36 +0100

    intégration docker et swagger

--- src/main/java/com/gestion_de_stock/produit/controller/ProduitController.java
+++ src/main/java/com/gestion_de_stock/produit/controller/ProduitController.java
@@ -1,5 +1,6 @@
 package com.gestion_de_stock.produit.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +14,10 @@ import lombok.RequiredArgsConstructor;
 public class ProduitController {
 
 	private ProduitService produitService;
+
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "Hello";
+	}
 
 }
