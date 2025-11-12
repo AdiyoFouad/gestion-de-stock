da1b64e9f3a502754233cc752632c5161c0c1b72
AdiyoFouad <odjfouad38@gmail.com>
2025-11-12 19:16:47 +0100

    Ajout des endpoint au CRUD

--- src/main/java/com/gestion_de_stock/produit/controller/ProduitController.java
+++ src/main/java/com/gestion_de_stock/produit/controller/ProduitController.java
@@ -2,8 +2,11 @@ package com.gestion_de_stock.produit.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,5 +31,20 @@ public class ProduitController {
 	@PostMapping("/add")
 	public Produit createProduit(@RequestBody Produit p) {
 		return produitService.createProduit(p);
+	}
+
+	@GetMapping("{id}")
+	public Produit getProduitById(@PathVariable long id) {
+		return produitService.getProduitById(id);
+	}
+
+	@DeleteMapping("{id}")
+	public String deleteProduitById(@PathVariable long id) {
+		return produitService.deleteProduitById(id);
+	}
+
+	@PutMapping("{id}")
+	public Produit editProduit(@PathVariable long id, @RequestBody Produit p) {
+		return produitService.editProduit(id, p);
 	}
 }
--- src/main/java/com/gestion_de_stock/produit/service/ProduitService.java
+++ src/main/java/com/gestion_de_stock/produit/service/ProduitService.java
@@ -1,6 +1,7 @@
 package com.gestion_de_stock.produit.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -23,5 +24,42 @@ public class ProduitService {
 	public Produit createProduit(Produit p) {
 		// TODO Auto-generated method stub
 		return produitRepository.save(p);
+	}
+
+	public Produit getProduitById(long id) {
+		Optional<Produit> optionalProduit = produitRepository.findById(id);
+
+		if (optionalProduit.isEmpty()) {
+			throw new RuntimeException("Produit inexistant");
+		}
+		return optionalProduit.get();
+	}
+
+	public String deleteProduitById(long id) {
+		Optional<Produit> optionalProduit = produitRepository.findById(id);
+
+		if (optionalProduit.isEmpty()) {
+			throw new RuntimeException("Suppression impossible : Produit inexistant");
+		}
+		produitRepository.delete(optionalProduit.get());
+
+		return "Produit supprimé avec succès !";
+
+	}
+
+	public Produit editProduit(long id, Produit p) {
+
+		Optional<Produit> optionalProduit = produitRepository.findById(id);
+
+		if (optionalProduit.isEmpty()) {
+			throw new RuntimeException("Modification impossible");
+		}
+
+		Produit produitAModifier = optionalProduit.get();
+
+		produitAModifier.setName(p.getName());
+		produitAModifier.setPrice(p.getPrice());
+
+		return produitRepository.save(produitAModifier);
 	}
 }
